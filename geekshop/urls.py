6093e1fb65b9530a4af3047272733295ac493497
Comradgrimo <39511425+Comradgrimo@users.noreply.github.com>
2021-02-12 17:33:29 +0300

    delete errors urls.py

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -11,5 +11,8 @@ urlpatterns = [
     path("", mainapp.main, name="main"),
     path("products/", include("mainapp.urls", namespace="products")),
     path("contact/", mainapp.contact, name="contact"),
+]
+
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
